cc8e3d58f80cdbe903e2e13069623f52f3ee9e2f
zsachs <Zachariah.Sachs@gmail.com>
2016-03-29 11:22:34 -0500

    Clean Player and its tests for readability.

--- player.rb
+++ player.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+
 class Player
+
 	attr_accessor :marker
 
 	def initialize(marker)
@@ -14,6 +16,7 @@ end
 
 
 class Human < Player
+
 	attr_accessor :name
 
 	def initialize(marker, name)
@@ -22,21 +25,24 @@ class Human < Player
 	end
 
 	def move
-		puts "Make your next move #{self.name}."
+		puts "Make your next move #{name}."
 		$stdin.gets.chomp.to_i
 	end
 end
 
 class Computer < Player
+
 	def move(node)
 		puts "It is the machine's turn to move."
-		# a computer makes moves based on the state of the current board.
-		# here say the computer is the maximizing_player
+# 		a computer moves based on the current board
+# 		make computer the maximizing_player
 		moves = node.valid_spots
 		scores = []
+
 		children(node, true).each do |child|
 			scores.push(minimax(child, 1, false))
 		end
+
 		best_score_index = scores.each_with_index.max[1]
 		spot = moves[best_score_index]
 		return spot
@@ -47,17 +53,20 @@ class Computer < Player
 	end
 
 	def children(node, maximizing_player)
-		opponent = (self.marker == 'X' ? 'O' : 'X')
+		opponent = (marker == 'X' ? 'O' : 'X')
 		kids = []
+
 		node.valid_spots.each do |spot|
 			board_clone = Board.new(:board => node.board.clone)
+
 			if maximizing_player
-				board_clone.mark(spot + 1, self.marker)
+				board_clone.mark(spot + 1, marker)
 			else
 				board_clone.mark(spot + 1, opponent)
 			end
 			kids.push(board_clone)
 		end
+
 		return kids
 	end
 
@@ -71,14 +80,17 @@ class Computer < Player
 		end
 
 		best_score = -1 * scores_for_minimax(0, maximizing_player)
+
 		children(node, maximizing_player).each do |child|
 			if maximizing_player
 				val = minimax(child, depth + 1, false)
+
 				if val > best_score
 					best_score = val
 				end
 			else
 				val = minimax(child, depth + 1, true)
+
 				if val < best_score
 					best_score = val
 				end
