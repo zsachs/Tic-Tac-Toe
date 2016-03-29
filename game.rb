e0c419972d509d5d13785545a684b11bcff48c51
zsachs <Zachariah.Sachs@gmail.com>
2016-03-29 11:22:02 -0500

    Clean Game and its tests for readability.

--- game.rb
+++ game.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+
 class Game
+
 	attr_accessor :grid, :turn, :player_one, :player_two
 
 	def initialize
@@ -12,10 +14,12 @@ class Game
 	def get_players
 		puts "How many of you want to play today?"
 		players = $stdin.gets.chomp.to_i
+
 		until players > 0
 			puts "Please enter a positive integer."
 			players = $stdin.gets.chomp.to_i
 		end
+
 		return players
 	end
 
@@ -24,16 +28,16 @@ class Game
 			puts "Looks like we've got a tournament."
 			puts "Two of you decide who plays 'X' and who plays 'O'."
 			puts "Who will play 'X'?"
-			one = Human.new('X', $stdin.gets.chomp)
+			self.player_one = Human.new('X', $stdin.gets.chomp)
 			puts "And who is the opponent?"
-			two = Human.new('O', $stdin.gets.chomp)
+			self.player_two = Human.new('O', $stdin.gets.chomp)
 			puts "It's on like Donkey Kong!"
 		elsif number == 2
 			puts "Alright."
 			puts "Who will play 'X'?"
-			one = Human.new('X', $stdin.gets.chomp)
+			self.player_one = Human.new('X', $stdin.gets.chomp)
 			puts "And who is the opponent?"
-			two = Human.new('O', $stdin.gets.chomp)
+			self.player_two = Human.new('O', $stdin.gets.chomp)
 			puts "It's on like Donkey Kong!"
 		elsif number == 1
 			puts "What is your name?"
@@ -41,17 +45,18 @@ class Game
 			puts "Your challenge has been accepted #{name}."
 			puts "Would you like to play 'X' or 'O'?"
 			marker = $stdin.gets.chomp.upcase
+
 			until marker == 'X' || marker == 'O'
 				puts "Please enter 'X' or 'O'."
 				marker = $stdin.gets.chomp.upcase
 			end
 
 			if marker == 'X'
-				one = Human.new('X', name)
-				two = Computer.new('O')
+				self.player_one = Human.new('X', name)
+				self.player_two = Computer.new('O')
 			elsif marker == 'O'
-				one = Computer.new('X')
-				two = Human.new('O', name)
+				self.player_one = Computer.new('X')
+				self.player_two = Human.new('O', name)
 			else
 				puts "ERROR: marker didn't set right."
 				exit(1)
@@ -60,7 +65,6 @@ class Game
 			puts "ERROR: needs an integer."
 			exit(1)
 		end
-		return [one, two]
 	end
 
 	def switch_turn
@@ -68,43 +72,47 @@ class Game
 	end
 
 	def next_move
-		valid = self.grid.valid_spots
+		valid = grid.valid_spots
 
-		if self.turn > 0
-			if self.player_one.instance_of? Human
-				self.grid.display
-				spot = self.player_one.move
+		if turn > 0
+			if player_one.instance_of? Human
+				grid.display
+				spot = player_one.move
+
 				until valid.include?(spot-1)
 					puts "Please choose a valid unmarked spot."
-					spot = self.player_one.move
+					spot = player_one.move
 				end
-				self.grid.mark(spot, self.player_one.marker)
+
+				grid.mark(spot, player_one.marker)
 			else
-				spot = self.player_one.move(self.grid)
+				spot = player_one.move(grid)
 				puts "The machine has moved to #{spot + 1}"
-				self.grid.mark(spot + 1, self.player_one.marker)
+				grid.mark(spot + 1, player_one.marker)
 			end
 		else
-			if self.player_two.instance_of? Human
-				self.grid.display
-				spot = self.player_two.move
+			if player_two.instance_of? Human
+				grid.display
+				spot = player_two.move
+
 				until valid.include?(spot-1)
 					puts "Please choose a valid unmarked spot."
-					spot = self.player_two.move
+					spot = player_two.move
 				end
-				self.grid.mark(spot, self.player_two.marker)
+
+				grid.mark(spot, player_two.marker)
 			else
-				spot = self.player_two.move(self.grid)
+				spot = player_two.move(grid)
 				puts "The machine has moved to #{spot + 1}"
-				self.grid.mark(spot + 1, self.player_two.marker)
+				grid.mark(spot + 1, player_two.marker)
 			end
 		end
 
-		self.switch_turn
+		switch_turn
 	end
 
 	def gameover
-		if self.grid.win? || self.grid.draw?
+		if grid.win? || grid.draw?
 			true
 		else
 			false
@@ -113,32 +121,32 @@ class Game
 
 	def start
 		puts "Welcome to Tic-Tac-Toe."
-		players = self.get_players
-		self.player_one, self.player_two = self.set_players(players)
+		players = get_players
+		set_players(players)
 
-		until self.gameover
-			self.next_move
+		until gameover
+			next_move
 		end
 
-		if self.grid.draw?
+		if grid.draw?
 			puts "You'll have to play again to see who takes out the trash."
 		else
-			if self.turn < 0
-				if self.player_one.instance_of? Human
-					puts "You've won, #{self.player_one.name}!"
+			if turn < 0
+				if player_one.instance_of? Human
+					puts "You've won, #{player_one.name}!"
 				else
 					puts "The machines grow stronger."
 				end
 			else
-				if self.player_two.instance_of? Human
-					puts "You've won, #{self.player_two.name}!"
+				if player_two.instance_of? Human
+					puts "You've won, #{player_two.name}!"
 				else
 					puts "The machines grow stronger."
 				end
 			end
-			self.grid.display
-		end
 
+			grid.display
+		end
 	end
 
 end
